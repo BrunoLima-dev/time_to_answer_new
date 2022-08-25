afc02d360ee903ac4389f0216ce1fed83e664724
BrunoLima-dev <brunoprograme04@gmail.com>
2022-08-25 19:00:05 -0300

    Adiciono ajuste menu lateral

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -19,7 +19,7 @@ Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_ba
 # /lib/assets
 Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css 
                                                   custom.js custom.css 
-                                                  img.jpg eriko.jpg)
+                                                  img.jpg eriko.jpg bruno.png)
 
 # /vendor/assets
 Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js )
