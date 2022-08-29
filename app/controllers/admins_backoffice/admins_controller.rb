de0fa7046409beec5726b88aa6e3537f079f9e57
BrunoLima-dev <brunoprograme04@gmail.com>
2022-08-29 19:57:50 -0300

    Criei uma nova arquivo new, rota para new, botão para new admin, def new

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -3,11 +3,23 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   before_action :set_admin, only: [:edit, :update] # Setar o admin antes de edit e update
 
   def index
-    @admins = Admin.all # Recebe todos os admins com os seus valores
+    @admins = Admin.all  # Recebe todos os admins com os seus valores
   end
 
-  def edit
+  def new
+    @admin = Admin.new
   end
+
+  def create
+    @admin = Admin.new(params_admin)
+    if @admin.save
+      redirect_to admins_backoffice_admins_path, notice: "Administrador cadastrado com sucesso!"
+    else
+      render :new
+    end
+  end
+
+  def edit; end
 
   def update
     if @admin.update(params_admin)
